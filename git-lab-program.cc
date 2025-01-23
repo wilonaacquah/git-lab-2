4e8ff8d86380b4aaa8685c19d9d52ad0d52cabb8
Wilona Acquah <wa623021@ohio.edu>
2025-01-22 19:24:27 -0500

    Created git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Wilona Acquah>
+ *        Date: <January 22 2025>
+ * Description: Lab 2: setting up Github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" <<endl;
+    return 0;
+}// main
